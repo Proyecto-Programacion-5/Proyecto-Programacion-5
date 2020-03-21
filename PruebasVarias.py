00b50a89b631cf862fa0b42a09f8b2f69c544622
juannico007 <58712333+juannico007@users.noreply.github.com>
2020-03-21 17:08:54 -0500

    Update PruebasVarias.py

--- PruebasVarias.py
+++ PruebasVarias.py
@@ -2,16 +2,18 @@ import random
 
 def HacerCuadricula(a):
     c = []
-    minas = []
     for i in range(1, a + 1):
         for j in range(1, a + 1):
             c.append((i,j))
+            
+def Colocarminas(c):
     rng = random.Random()
     print(len(c))
     for i in range(len(c) // 5):
         Mina = rng.randrange(0, len(c))
         c[Mina] = (c[Mina], "mina")
-    h = 0
+        
+def ContarCuadros(c):
     comp = 0
     for i in c:
         if "mina" not in i:
@@ -30,14 +32,7 @@ def HacerCuadricula(a):
             if "mina" in (i[0],i[1]+1):
                 comp += 1
         
-
-            
-            
-    for i in c:
-        if "mina" not in i:
-           c[h] = (c[h], N_minas)
-        h += 1
-    return c
+return c
 
     
 print(HacerCuadricula(10))
