777aedbb5dcc043ecf051b6b13b3268fe153e032
juannico007 <58712333+juannico007@users.noreply.github.com>
2020-03-21 16:24:30 -0500

    Update PruebasVarias.py

--- PruebasVarias.py
+++ PruebasVarias.py
@@ -1,9 +1,16 @@
+import random
+
 def HacerCuadricula(a):
     c = []
     for i in range(1, a + 1):
         for j in range(1, a + 1):
             c.append((i,j))
+    rng = random.Random()
+    print(len(c))
+    for i in range(len(c) // 5):
+        Mina = rng.randrange(0, len(c))
+        c[Mina] = (c[Mina], 1)
     return c
 
-print(HacerCuadricula(2))
-
+    
+print(HacerCuadricula(10))
