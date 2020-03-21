b222ae6ee3acee82803821e80e5af8ce427c212a
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2020-03-21 18:02:11 -0500

    Inclusión de diccionarios, para corrección de errores de intención

--- PruebasVarias.py
+++ PruebasVarias.py
@@ -8,17 +8,24 @@ def HacerCuadricula(a):
     return c
             
 def Colocarminas(c):
+    c_dict = {}
+    for i in c:
+        c_dict[i] = c_dict.get(i, False)
     rng = random.Random()
-    print(len(c))
     for i in range(len(c) // 5):
         Mina = rng.randrange(0, len(c))
-        c[Mina] = (c[Mina], "mina")
-    return c
+        # c[Mina] = (c[Mina], "mina")
+        c_dict[c[Mina]] = c_dict.get(i, True)
+    print(c_dict)
+
+    return c_dict, c
         
 def ContarCuadros(c):
     comp = 0
+    h = 0
     for i in c:
-        if "mina" not in i:
+        
+        if c[i] == False:
             if "mina" in (i[0]+1,i[1]):
                 comp += 1
             if "mina" in (i[0]+1,i[1]+1):
@@ -33,13 +40,16 @@ def ContarCuadros(c):
                 comp += 1
             if "mina" in (i[0],i[1]+1):
                 comp += 1
-            
+        c[h] = (c[h], comp)
+        h += 1    
+    return c
         
 
 
 
 #BOE
-cuadricula = HacerCuadricula(10)
-minas = Colocarminas(cuadricula)
-cuadricula2 = ContarCuadros(cuadricula)
+cuadricula = HacerCuadricula(5)
+minas_d, minas = Colocarminas(cuadricula)
+cuadricula2 = ContarCuadros(minas_d, minas)
+print(cuadricula2)
 #EOE
