bcfefbc463cc272e6f7055b55498abe75b9fcd95
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2020-03-21 17:06:02 -0500

    Update PruebasVarias.py

--- PruebasVarias.py
+++ PruebasVarias.py
@@ -2,6 +2,7 @@ import random
 
 def HacerCuadricula(a):
     c = []
+    minas = []
     for i in range(1, a + 1):
         for j in range(1, a + 1):
             c.append((i,j))
@@ -9,7 +10,33 @@ def HacerCuadricula(a):
     print(len(c))
     for i in range(len(c) // 5):
         Mina = rng.randrange(0, len(c))
-        c[Mina] = (c[Mina], 1)
+        c[Mina] = (c[Mina], "mina")
+    h = 0
+    comp = 0
+    for i in c:
+        if "mina" not in i:
+            if "mina" in (i[0]+1,i[1]):
+                comp += 1
+            if "mina" in (i[0]+1,i[1]+1):
+                comp += 1
+            if "mina" in (i[0],i[1]+1):
+                comp += 1
+            if "mina" in (i[0]-1,i[1]+1):
+                comp += 1
+            if "mina" in (i[0]-1,i[1]):
+                comp += 1
+            if "mina" in (i[0]-1,i[1]-1):
+                comp += 1
+            if "mina" in (i[0],i[1]+1):
+                comp += 1
+        
+
+            
+            
+    for i in c:
+        if "mina" not in i:
+           c[h] = (c[h], N_minas)
+        h += 1
     return c
 
     
