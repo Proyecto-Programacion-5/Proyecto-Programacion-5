8d27432b87ae005f8d736ddd8f2c28b3a063f840
juannico007 <58712333+juannico007@users.noreply.github.com>
2020-03-28 14:43:14 -0500

    Update posibleinterfaz1.py

--- posibleinterfaz1.py
+++ posibleinterfaz1.py
@@ -1,12 +1,8 @@
-def ordenarcuadricula(size, c):      
-    for i in c:    
+def ordenarcuadricula(size, c): 
+    a = 0
+    for i in range(size):
         for j in range(size):
-            print(i, end = ", ")
+            print(c[a], end = ", ")
+            a += 1
         print()
        
-            
-        
-        
-c = [2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1,2,3,4,5,6,7,8]
-size = 5
-ordenarcuadricula(size, c)
