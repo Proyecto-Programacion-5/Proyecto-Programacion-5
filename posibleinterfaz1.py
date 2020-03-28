45fd0bf4492617c934fb3488d1ec621a33e51ebf
juannico007 <58712333+juannico007@users.noreply.github.com>
2020-03-28 14:37:16 -0500

    Update posibleinterfaz1.py

--- posibleinterfaz1.py
+++ posibleinterfaz1.py
@@ -1,19 +1,12 @@
-from pprint import pprint
-
 def ordenarcuadricula(size, c):      
-    x = 0
-    y = size
-    e = []
-    for i in range(size):    
-        e.append((c[x:y]))
-        x += size
-        y += size
-        
-    return e
+    for i in c:    
+        for j in range(size):
+            print(i, end = ", ")
+        print()
+       
             
         
         
 c = [2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1,2,3,4,5,6,7,8]
 size = 5
-a = ordenarcuadricula(size, c)
-pprint(a)
+ordenarcuadricula(size, c)
