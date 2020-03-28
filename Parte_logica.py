86f80fbf41b6456b6d4637a716feb358022b92be
juannico007 <58712333+juannico007@users.noreply.github.com>
2020-03-28 17:20:02 -0500

    Update Parte_logica.py

--- Parte_logica.py
+++ Parte_logica.py
@@ -15,6 +15,7 @@ def Colocarminas(lista):
     for i in range(len(lista) // 5):
         Mina = rng.randrange(0, len(lista))
         minas_d[lista[Mina]] = minas_d.get(i, True)
+    print(minas_d)
     return minas_d
         
 def ContarCuadros(minas_d, cuadricula_ls):
@@ -42,61 +43,65 @@ def ContarCuadros(minas_d, cuadricula_ls):
             if (y[0]-1,y[1]-1) in minas_d:
                 if minas_d[(y[0]-1,y[1]-1)] == True:
                     minasC += 1
-            if (y[0],y[1]+1) in minas_d:
-                if minas_d[(y[0],y[1]+1)] == True:
-                    minasC += 1
-        minasA_dt[i] = minasA_dt.get(i, minasC)
+            if (y[0],y[1]-1) in minas_d:
+                if minas_d[(y[0],y[1]-1)] == True:
+                    minasC += 1 
+            if (y[0]+1,y[1]-1) in minas_d:
+                if minas_d[(y[0]+1,y[1]-1)] == True:
+                    minasC += 1 
+            minasA_dt[i] = minasA_dt.get(i, minasC)
         counter += 1
+    print(minasA_dt)
     return (minasA_dt)
 
-def ejecucion(dt, dt2, ls):
+def ejecucion(minas_dt, minasA_dt, cuadricula, size):
     while True:
         coord_x = int(input("ingrese una coordenada en x:"))
         coord_y = int(input("ingrese una coordenada en y:"))
         coordenada = (coord_x, coord_y)
-        for i in ls:
+        for i in cuadricula:    
             if i == coordenada:
                 y = i
-
-        if (y[0]+1, y[1]) in dt2:
-            a = dt2[(y[0]+1, y[1])]
-        if (y[0]+1, y[1]+1) in dt2:
-            b = dt2[(y[0]+1, y[1]+1)]
-        if (y[0], y[1]+1) in dt2:
-            c = dt2[(y[0], y[1]+1)]
-        if (y[0]-1, y[1]+1) in dt2:
-            d = dt2[(y[0]-1, y[1]+1)]
-        if (y[0]-1, y[1]) in dt2:
-            e = dt2[(y[0]-1, y[1])]
-        if (y[0]-1, y[1]-1) in dt2:
-            f = dt2[(y[0]-1, y[1]-1)]
-        if (y[0], y[1]-1) in dt2:
-            g = dt2[(y[0], y[1]-1)]
-        if (y[0]+1, y[1]-1) in dt2:
-            h = dt2[(y[0]+1, y[1]-1)]
-
-        if dt[coordenada] == True:
+                if (y[0]+1, y[1]) in minasA_dt:
+                    a = minasA_dt[(y[0]+1, y[1])]
+                if (y[0]+1, y[1]+1) in minasA_dt:
+                    b = minasA_dt[(y[0]+1, y[1]+1)]
+                if (y[0], y[1]+1) in minasA_dt:
+                    c = minasA_dt[(y[0], y[1]+1)]
+                if (y[0]-1, y[1]+1) in minasA_dt:
+                    d = minasA_dt[(y[0]-1, y[1]+1)]
+                if (y[0]-1, y[1]) in minasA_dt:
+                    e = minasA_dt[(y[0]-1, y[1])]
+                if (y[0]-1, y[1]-1) in minasA_dt:
+                    f = minasA_dt[(y[0]-1, y[1]-1)]
+                if (y[0], y[1]-1) in minasA_dt:
+                    g = minasA_dt[(y[0], y[1]-1)]
+                if (y[0]+1, y[1]-1) in minasA_dt:
+                    h = minasA_dt[(y[0]+1, y[1]-1)]
+        if minas_dt[coordenada] == True:
             print("GAME OVER")
             return
-        else:
-            print(dt2[coordenada])
-            """print("(1,0):", a)
-            print("(1,1):", b)
-            print("(0,1):", c)
-            print("(-1,1):", d)
-            print("(-1,0):", e)
-            print("(-1,-1):", f)
-            print("(0,-1):", g)
-            print("(1,-1):", h)"""
+        else: 
+            uncover(cuadricula, coordenada, minasA_dt, size)
+           
         
         
-def MostrarCuadricula(size, c): 
-    a = 0
+def MostrarCuadricula(size, cuadricula): 
+    counter = 0
     for i in range(size):
         for j in range(size):
-            print(c[a], end = " ")
-            a += 1
+            print(cuadricula[counter], end = " ")
+            counter += 1
         print()
+        
+def uncover(cuadricula, coordenada, minasA_dt, sz):
+    for i in range(len(cuadricula)):
+        if cuadricula[i] == coordenada:
+            cuadricula[i] = "  {0}   ".format(minasA_dt.get(coordenada))
+            MostrarCuadricula(sz, cuadricula)
+            break
+    return cuadricula
+
 
 #BOE
 
@@ -106,6 +111,7 @@ cuadricula_ls = HacerCuadricula(sz)
 minas_dt = Colocarminas(cuadricula_ls)
 minasA_dt = ContarCuadros(minas_dt, cuadricula_ls)
 MostrarCuadricula(sz, cuadricula_ls)
-ejecucion(minas_dt, minasA_dt, cuadricula_ls)
+
+ejecucion(minas_dt, minasA_dt, cuadricula_ls, sz)
 
 #EOE
