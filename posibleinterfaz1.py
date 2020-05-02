7d9fe14b99ed71fbf9e3e311012f7e6519d26cf1
DarkfireT09 <62482962+DarkfireT09@users.noreply.github.com>
2020-05-02 17:27:27 -0500

    Delete posibleinterfaz1.py

--- posibleinterfaz1.py
+++
@@ -1,8 +0,0 @@
-def ordenarcuadricula(size, c): 
-    a = 0
-    for i in range(size):
-        for j in range(size):
-            print(c[a], end = " ")
-            a += 1
-        print()
-       
