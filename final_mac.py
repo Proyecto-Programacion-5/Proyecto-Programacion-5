907635aa11c8e3846e0ebdcf892290d2a8c22866
juannico007 <58712333+juannico007@users.noreply.github.com>
2020-05-02 20:26:47 -0500

    Update final_mac.py

--- final_mac.py
+++ final_mac.py
@@ -198,7 +198,7 @@ class Juego:
                     self.cuadricula.append(i)                                                                       #Vuelve a incluir el cuadro en la lista de cuadricula
                     self.banderas_restantes +=1                                                                     #Le suma 1 a las banderas restantes
                     self.estado[i] = False                                                                          #Actualiza el estado a no tener bandera
-                    if self.minasA_dt[i] == True:
+                    if str(self.minasA_dt[i]) == "True":                                                            #Compara con el valor de la mina, se debe convertir a str para que no cuente los 1 como true
                         self.cont_band -=1
                     break
                 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -227,7 +227,7 @@ class Juego:
                             a = self.minasA_dt[(i[0] + self.width + 5 ,i[1] + self.width + 5 )]
                             self.uncover((i[0] + self.width + 5 ,i[1] + self.width + 5 ))
                             if a == 0:
-                                self.ejecucion((i[0] + self.width + 5 ,i[1] + self.width + 5 ))    #Si el valor de la casilla es 0 repite la ejecucion con esa casilla     
+                                self.ejecucion((i[0] + self.width + 5 ,i[1] + self.width + 5 ))     #Si el valor de la casilla es 0 repite la ejecucion con esa casilla     
                         if (i[0], i[1] + self.width + 5 ) in self.minasA_dt:
                             a = self.minasA_dt[(i[0], i[1] + self.width + 5 )]
                             self.uncover((i[0], i[1] + self.width + 5 ))
@@ -247,92 +247,89 @@ class Juego:
                             a = self.minasA_dt[(i[0] - self.width - 5 ,i[1] - self.width - 5 )]
                             self.uncover((i[0] - self.width - 5 ,i[1] - self.width - 5 ))
                             if a == 0:
-                                self.ejecucion((i[0] - self.width - 5 ,i[1] - self.width - 5 ))                
+                                self.ejecucion((i[0] - self.width - 5 ,i[1] - self.width - 5 ))     #Si el valor de la casilla es 0 repite la ejecucion con esa casilla   
                         if (i[0], i[1] - self.width - 5 ) in self.minasA_dt:
                             a = self.minasA_dt[(i[0], i[1] - self.width - 5 )]
                             self.uncover((i[0], i[1] - self.width - 5 ))
                             if a == 0:
-                                self.ejecucion((i[0], i[1] - self.width - 5 ))
+                                self.ejecucion((i[0], i[1] - self.width - 5 ))                      #Si el valor de la casilla es 0 repite la ejecucion con esa casilla
                         if (i[0] + self.width + 5 ,i[1] - self.width - 5 ) in self.minas_d:
                             a = self.minasA_dt[(i[0] + self.width + 5 ,i[1] - self.width - 5 )]
                             self.uncover((i[0] + self.width + 5 ,i[1] - self.width - 5 ))
                             if a == 0:
-                                self.ejecucion((i[0] + self.width + 5 ,i[1] - self.width - 5 ))
-
+                                self.ejecucion((i[0] + self.width + 5 ,i[1] - self.width - 5 ))     #Si el valor de la casilla es 0 repite la ejecucion con esa casilla
+                    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
                 break
-
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
-    #Metodo que muestra el tiempo que lleva el usuario
+#---------------------- Metodo que muestra el tiempo que lleva el usuario ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def ImpTiempo(self, t1):
-        pos = (self.centro[0] - 100, self.centro[1] + (self.width + 5) * (self.size/2) + 15)
+        pos = (self.centro[0] - 200, self.centro[1] + (self.width + 5) * (self.size/2) + 15)        #Posicion del tiempo: Abajo en el centro
         t2 = time.perf_counter()
         t = str(int(t2-t1))
-        t = self.temp.render("Tiempo : {0} sec".format(t), 1, (255,255,0))        
-        pygame.draw.rect(self.ventana, (0,0,0), (pos[0], pos[1], self.width * 20, self.width))
-        self.ventana.blit(t, (pos))
+        t = self.temp.render("Tiempo : {0} sec".format(t), 1, (255,255,0))                          #Crea el mensae que muestra el tiempo
+        pygame.draw.rect(self.ventana, (0,0,0), (pos[0], pos[1], 200, self.width))                  #Dibuja un fondo negro para que no se sobreponga
+        self.ventana.blit(t, (pos))                                                                 #Dibuja el mensaje
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
             
-    #Metodo que muestra la solucion si el jugador pierde
+#---------------------- Metodo que muestra la solucion si el jugador pierde -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def MostrarRespuesta(self):
         for i in self.minas_d:
             if self.minas_d[i] == True:
                 pos = [i[0], i[1]]
                 self.ventana.blit(self.mina, pos)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
             
-            
-            
-            
-#BOE
+
+
+#----------------------------- Bloque de ejecucion --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 pygame.init()
+m = 0                                                                                                               #Variable que regula el metodo safe
+juego = Juego(700,600)                                                                                              #Inicializa el juego
+t1 = time.perf_counter()                                                                                            #Obtiene el momento en que se inicio el juego
+juega = True                                                                                                        #Permite leer o no las acciones sobre el mapa
+pos_arriba = (juego.centro[0] - 75, juego.centro[1] - (juego.width + 5) * (juego.size/2) - 40)                      #Posicion de los mensajes arriba de la cuadricula
+pos_abajo = (juego.centro[0], juego.centro[1] + (juego.width + 5) * (juego.size/2) + 15)                            #Posicion de los mensajes abajo de la cuadricula
 
-
-m = 0
-juego = Juego(700,600)
-t1 = time.perf_counter()
-clock = pygame.time.Clock()
-juega = True
-
-run = True
+run = True                                                                                                          #Regula el ciclo
+#----------------------------- Mainloop -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 while run:    
-    clock.tick(60)
-    
-    juego.MostrarCuadricula()
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        run = False
-    if event.type == pygame.MOUSEBUTTONDOWN and juega:
-        pos = event.dict['pos']
-        if event.button == 1:
-            if m == 0:
-                m = juego.safe(pos)
+
+    juego.MostrarCuadricula()                                                                   
+    event = pygame.event.poll()                                                                                     #Lee los eventos
+    if event.type == pygame.QUIT:   
+        run = False                                                                                                 #Termina el ciclo
+    if event.type == pygame.MOUSEBUTTONDOWN and juega:  
+        pos = event.dict['pos']                                                                                     #Lee la posicion del mouse
+        if event.button == 1:   
+            if m == 0:  
+                m = juego.safe(pos)                                                                                 #Ejecuta el metodo safe, si se ejecuta adecuadamente retorna 1 y no se vuelve a hacer
             juego.ejecucion(pos)
         if event.button == 3:
             juego.PonerOQuitarBandera(pos)
 
-
-    if juego.cont_band == juego.banderas_max:
-        msg = juego.msg.render("Victoria", 1, (255,255,0))
-        pos = (juego.centro[0] - 75, juego.centro[1] - (juego.width + 5) * (juego.size/2) - 40)
-        pygame.draw.rect(juego.ventana, (0,0,0), (pos[0], pos[1], juego.width * 20, 30))
-        juego.ventana.blit(msg, (pos[0], pos[1]))
-        t2 = time.perf_counter()
-        pygame.display.update()
-        juega = False
+    msg_banderas = juego.temp.render("Banderas: {0}".format(juego.banderas_restantes), 1, (255,255,0))               #Mensaje de las banderas restantes
+    pygame.draw.rect(juego.ventana, (0,0,0), (pos_abajo[0] + 100, pos_abajo[1], juego.width * 20, 30))              #Fondo del mensaje
+    juego.ventana.blit(msg_banderas, (pos_abajo[0] + 100,pos_abajo[1]))                                             #Imprime el mensaje
+    
+    if juego.cont_band == juego.banderas_max:                                                                       #Si todas las banderas estan bien colodadas
+        msg = juego.msg.render("Victoria", 1, (255,255,0))                                                          #Mensaje de victoria
+        pygame.draw.rect(juego.ventana, (0,0,0), (pos_arriba[0], pos_arriba[1], juego.width * 10, 30))               #Imprime el fondo del mensaje
+        juego.ventana.blit(msg, pos_arriba)                                                                         #Imprime el mensaje
+        pygame.display.update()                                                                                     #Actualiza por ultima vez
+        juega = False                                                                                               #Hace que deje de leer clicks en la cuadricula
         
-    elif not juego.est:
-        msg = juego.msg.render("Pierde", 1, (255,255,0))
-        pos = (juego.centro[0] - 75, juego.centro[1] - (juego.width + 5) * (juego.size/2) - 40)
-        pygame.draw.rect(juego.ventana, (0,0,0), (pos[0], pos[1], juego.width * 20, 30))
-        juego.ventana.blit(msg, (pos[0], pos[1]))
+    elif not juego.est:                                                                                             #Si clickeo una mina
+        msg = juego.msg.render("Pierde", 1, (255,255,0))                                                            #Mensaje de derrota
+        pygame.draw.rect(juego.ventana, (0,0,0), (pos_arriba[0], pos_arriba[1], juego.width * 10, 30))               #Imprime el fondo del mensaje
+        juego.ventana.blit(msg, pos_arriba)                                                                         #Imprime el mensaje
         juego.MostrarRespuesta()
-        t2 = time.perf_counter()
-        pygame.display.update()
-        juega = False
+        pygame.display.update()                                                                                     #Actualiza por ultima vez
+        juega = False                                                                                               #Hace que deje de leer clicks en la cuadricula
 
     else:
         juego.ImpTiempo(t1)
-
         pygame.display.update()
-
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 pygame.quit()
-
-##EOE
+#------------------------------ Fin de la ejecucion -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
